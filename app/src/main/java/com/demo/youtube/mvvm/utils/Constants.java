163f09c8907937ff3960ed6ccca237f9c822074b
Shivanand <shivuas123@gmail.com>
2019-07-08 15:12:24 +0530

    Update Constants.java

--- app/src/main/java/com/demo/youtube/mvvm/utils/Constants.java
+++ app/src/main/java/com/demo/youtube/mvvm/utils/Constants.java
@@ -4,8 +4,8 @@ package com.demo.youtube.mvvm.utils;
  * Created by ${Shivanand} on 7/8/2019.
  */
 public class Constants {
-    public static String API_KEY="AIzaSyD9_jktgSHHg6E42PFeUrUte5YCHeRHpzw";
-    public static String CHANNEL_ID="UC1NF71EwP41VdjAU1iXdLkw";
+    public static String API_KEY="";
+    public static String CHANNEL_ID="";
     public static String PART ="snippet";
     public static int ERROR_400=400;
     public static int ERROR_403=403;
